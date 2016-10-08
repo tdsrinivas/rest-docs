fd10e02fab576f1ae72b3bad11120f7eec1d90ad
Srinivas Raj <srinivas@Srinivass-MacBook-Pro.local>
2016-10-08 09:25:29 -0600

    first commit

---
+++ src/main/java/com/raj/RestDocsApplication.java
@@ -0,0 +1,12 @@
+package com.raj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestDocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestDocsApplication.class, args);
+	}
+}
